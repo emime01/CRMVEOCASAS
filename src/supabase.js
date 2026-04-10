765549597dc7bd370ab0b3c798618087010b8e8b
emime01 <emiliano@movimagen.com>
2026-04-10 03:29:01 -0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://nrpgykebktypuvidovsz.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_GphjR_dNdLY77M92KY4D5w_Hajfdiaq'
+const SUPABASE_KEY = 'sb_secret_9F1gFDoAAp6ZdQ8rQXT7WQ_DcsYZA8K'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
+  auth: { persistSession: false }
+})
