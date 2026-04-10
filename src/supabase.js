5e45c9078ca8e237979d49b06ad48ffdd376da9b
emime01 <emiliano@movimagen.com>
2026-04-10 04:39:17 -0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
-const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+export const supabase = createClient(
+  'https://nrpgykebktypuvidovsz.supabase.co',
+  'sb_publishable_GphjR_dNdLY77M92KY4D5w_Hajfdiaq'
+)
