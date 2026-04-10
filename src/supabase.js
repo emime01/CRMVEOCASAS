ea7bb095cc8362eddfeb0eaa7d2f3ba79b620463
emime01 <emiliano@movimagen.com>
2026-04-10 03:52:18 -0300

    Delete src/supabase.js

--- src/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://nrpgykebktypuvidovsz.supabase.co'
-const SUPABASE_KEY = 'sb_secret_9F1gFDoAAp6ZdQ8rQXT7WQ_DcsYZA8K'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
-  auth: { persistSession: false }
-})
